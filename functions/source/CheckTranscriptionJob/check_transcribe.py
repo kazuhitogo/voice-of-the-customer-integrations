3a2aa45f4d6e5b03a5ba7b2647bd70cc4c43acf3
kazuhitogo <kazuhitogo@gmail.com>
2020-09-15 17:35:25 +0900

    add agent name and arn, sentence detect sentiment

--- functions/source/CheckTranscriptionJob/check_transcribe.py
+++ functions/source/CheckTranscriptionJob/check_transcribe.py
@@ -1,7 +1,37 @@
+from aws_requests_auth.aws_auth import AWSRequestsAuth
+from elasticsearch import Elasticsearch, RequestsHttpConnection
+import os,boto3,certifi,boto3,json
 
-import boto3
-import json
-
+REGION = os.getenv('AWS_REGION')
+esendpoint = os.environ['ES_DOMAIN']
+FULL_EPISODE_INDEX = os.getenv('ES_EPISODE_INDEX', default='ctr')
+FULL_EPISODE_DOCTYPE = os.getenv('FULL_EPISODE_DOCTYPE', default='doc')
+session = boto3.session.Session()
+credentials = session.get_credentials().get_frozen_credentials()
+awsauth = AWSRequestsAuth(
+    aws_access_key=credentials.access_key,
+    aws_secret_access_key=credentials.secret_key,
+    aws_token=credentials.token,
+    aws_host=esendpoint,
+    aws_region=REGION,
+    aws_service='es'
+)
+es = Elasticsearch(
+    hosts=[{'host': esendpoint, 'port': 443}],
+    http_auth=awsauth,
+    use_ssl=True,
+    verify_certs=True,
+    ca_certs=certifi.where(),
+    timeout=120,
+    connection_class=RequestsHttpConnection
+)
+def es_make_body(contact_id):
+    esbody = {
+        "_source": ["Agent"],
+        "query": {
+            "term":{"_id":{"value":contact_id}}}
+    }
+    return esbody
 
 # The entry point for the lambda function
 def lambda_handler(event, context):
@@ -11,18 +41,29 @@ def lambda_handler(event, context):
     client = boto3.client('transcribe')
 
     # Call the AWS SDK to get the status of the transcription job
-    response = client.get_transcription_job(TranscriptionJobName=transcribeJob)
+    transcribe_response = client.get_transcription_job(TranscriptionJobName=transcribeJob)
 
-    # Pull the status
-    status = response['TranscriptionJob']['TranscriptionJobStatus']
+    # Pull the transcribe job status
+    transcribe_job_status = transcribe_response['TranscriptionJob']['TranscriptionJobStatus']
 
     retval = {
-        "status": status
+        "status": transcribe_job_status
     }
 
     # If the status is completed, return the transcription file url. This will be a signed url
     # that will provide the full details on the transcription
-    if status == 'COMPLETED':
-        retval["transcriptionUrl"] = response['TranscriptionJob']['Transcript']['TranscriptFileUri']
+    if transcribe_job_status == 'COMPLETED':
+        # Pull the contact-id
+        contact_id = transcribe_response['TranscriptionJob']['Media']['MediaFileUri'].split('/')[-1].split('_')[0]
+        es_response = es.search(index=FULL_EPISODE_INDEX, body=es_make_body(contact_id))
+        if es_response['hits']['hits'] == []:
+            retval["status"] = 'agent-id not yet'
+        else:
+            # pull agent_name and agent_arn
+            retval["Username"] = es_response['hits']['hits'][0]['_source']['Agent']['Username']
+            retval["ARN"] = es_response['hits']['hits'][0]['_source']['Agent']['ARN']
+            retval["transcriptionUrl"] = transcribe_response['TranscriptionJob']['Transcript']['TranscriptFileUri']
+    print(retval)
+    print(type(retval))
 
     return retval
